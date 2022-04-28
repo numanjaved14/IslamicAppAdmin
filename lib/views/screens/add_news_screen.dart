5275d36363a931c16d1a9f83f03dc61f85460c8f
numanjaved14 <numanjaved14@gmail.com>
2022-04-28 18:25:53 +0500

    second commit

--- lib/views/screens/add_news_screen.dart
+++ lib/views/screens/add_news_screen.dart
@@ -26,6 +26,7 @@ class _AddNewsScreenState extends State<AddNewsScreen> {
   void dispose() {
     _titleController.dispose();
     _descriptionController.dispose();
+    loading = false;
     super.dispose();
   }
 
